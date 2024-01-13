7b609109bdeed007d349b4eab17e2c02d0f4e080
abcd <nextmachenic@gmail.com>
2024-01-13 11:55:03 +0530

    modify_7.Ceaser_Cipher

--- 4_functions/Practice_Questions/Fuctions_with_list/7.Ceaser_Cipher_automated.py
+++ 4_functions/Practice_Questions/Fuctions_with_list/7.Ceaser_Cipher_automated.py
@@ -9,18 +9,14 @@ any message(words,sentence,etc.)'''
 alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m',
              'n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-#take inputs(encode/decode,message,shift) from the user
-coding=input("Type 'encode' -> encrypt and 'decode' -> decrypt:\n")
-message=input('Enter your message:\n').lower()
-shift=int(input('Number of shifts:\n'))
-
 #create a function to encrypt the message
 def encrypt(simple_message,shift):
     encoded_message=''
     for a in simple_message:
+        #check if the 'char' is in 'alphabets' list, if yes find shifted_letter and add to encoded_text otherwise add 'char' itself 
         if a in alphabets:
-            shifted_word=(alphabets[(alphabets.index(a)+shift)%26])    #%26 is bcz, list has only range(0,26)
-            encoded_message+=shifted_word
+            shifted_letter=(alphabets[(alphabets.index(a)+shift)%26])    #%26 is bcz, list has only range(0,26)
+            encoded_message+=shifted_letter
         else:
             encoded_message+=a
     print(f"The encryted message is '{encoded_message}'")
@@ -29,18 +25,37 @@ def encrypt(simple_message,shift):
 def decrypt(encrypted_message,shift):
     decoded_message=''
     for a in encrypted_message:
+        #check if the 'char' is in 'alphabets' list, if yes find shifted_letter and add to encoded_text otherwise add 'char' itself
         if a in alphabets:
-            reshifted_word=(alphabets[(alphabets.index(a)-shift)%26])    #%26 is bcz, list has only range(0,26)
-            decoded_message+=reshifted_word
+            reshifted_letter=(alphabets[(alphabets.index(a)-shift)%26])    #%26 is bcz, list has only range(0,26)
+            decoded_message+=reshifted_letter
         else:
             decoded_message+=a
     print(f"The decrypted message is '{decoded_message}'")
 
-#check the user's coding(encode/decode) and print result accordingly
-if coding=='encode':
-    encrypt(simple_message=message,shift=shift)
-elif coding=='decode':
-    decrypt(encrypted_message=message,shift=shift)
+#create a loop asking user if he wants to continue or stop
+should_continue=True
+while should_continue:
+    #take inputs(encode/decode,message,shift) from the user
+    coding=input("Type 'encode' -> encrypt and 'decode' -> decrypt:\n")
+    message=input('Enter your message:\n').lower()
+    shift=int(input('Number of shifts:\n'))
+
+    #check the user's coding(encode/decode) and print result accordingly
+    if coding=='encode':
+        encrypt(simple_message=message,shift=shift)
+    elif coding=='decode':
+        decrypt(encrypted_message=message,shift=shift)
+
+    #if user says 'no' to continue end the loop    
+    user_choice=input("Want to continue? Type 'yes' or 'no'\n")
+    if user_choice=='yes':
+        continue
+    elif user_choice=='no':
+        should_continue=False
+    else:
+        print("Wrong choice!")
+        break
 
 #---------------------------------------------------------------------------------------------------------------#
 
@@ -50,11 +65,6 @@ elif coding=='decode':
 alphabets=['a','b','c','d','e','f','g','h','i','j','k','l','m',
              'n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-#take inputs(encode/decode,message,shift) from the user
-operation=input("Type 'encode' -> encrypt and 'decode' -> decrypt:\n")
-user_text=input('Enter your message:\n').lower()
-shift=int(input('Number of shifts:\n'))
-
 #create a function 'ceaser' to perform both encryption and decryption
 def ceaser(coding,message,shift):
     coded_text=''
@@ -62,11 +72,29 @@ def ceaser(coding,message,shift):
         shift*=-1
     for l in message:
         position=alphabets.index(l)
-        new_position=alphabets[(position+shift)%26]    #%26 is bcz, list has only range(0,26)
+        new_position=(position+shift)%26    #%26 is bcz, list has only range(0,26)
         coded_text+=alphabets[new_position]
     print(f"The {coding}d result is '{coded_text}'")
 
-#call the 'ceaser' function
-ceaser(coding=operation,message=user_text,shift=shift)
+#create a loop asking user if he wants to continue or stop
+should_continue=True
+while should_continue:
+    #take inputs(encode/decode,message,shift) from the user
+    operation=input("Type 'encode' -> encrypt and 'decode' -> decrypt:\n")
+    user_text=input('Enter your message:\n').lower()
+    shift=int(input('Number of shifts:\n'))
 
+    #call the 'ceaser' function
+    ceaser(coding=operation,message=user_text,shift=shift)
+    
+    #if user says 'no' to continue end the loop    
+    user_choice=input("Want to continue? Type 'yes' or 'no'\n")
+    if user_choice=='yes':
+        continue
+    elif user_choice=='no':
+        should_continue=False
+    else:
+        print("Wrong choice!")
+        break
+    
 #---------------------------------------------------------------------------------------------------------------#
